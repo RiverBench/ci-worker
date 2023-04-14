af34a76ddd7cca93f4c05fe514f97f2cfa6bc978
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2023-04-14 17:34:52 +0200

    Update badges... again!

--- src/main/scala/commands/DatasetDocGenCommand.scala
+++ src/main/scala/commands/DatasetDocGenCommand.scala
@@ -125,8 +125,7 @@ object DatasetDocGenCommand extends Command:
        |-- to them from the description in the metadata.ttl file.
        |--
        |-->
-       |[![.github/workflows/validate.yaml]($baseRepoUrl/dataset-$id/actions/workflows/validate.yaml/badge.svg?event=push)]($baseRepoUrl/dataset-$id/actions/workflows/validate.yaml)
-       |[![.github/workflows/release.yaml]($baseRepoUrl/dataset-$id/actions/workflows/release.yaml/badge.svg?event=push)]($baseRepoUrl/dataset-$id/actions/workflows/release.yaml)
+       |[![.github/workflows/ci.yaml]($baseRepoUrl/dataset-$id/actions/workflows/ci.yaml/badge.svg?event=push)]($baseRepoUrl/dataset-$id/actions/workflows/ci.yaml)
        |""".stripMargin
 
   private def readmeIntro(websiteLink: String): String =
